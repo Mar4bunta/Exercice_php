505d2fdb928923ea847ee04e7402f28d4491fecd
Erwan R <rocforterwan@yahoo.fr>
2020-06-22 13:24:24 +0200

    Tp6
    Formulaire tp 6

---
+++ Cours6/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    $civil = $_GET['select'];
+    $nom = $_GET['nom'];
+    $prenom = $_GET['prenom'];
+    $email = $_GET['email'];
+
+    echo "Civilité : ",$civil, "<br>Nom : ",$nom, "<br>Prénom : ",$prenom, "<br>adresse email : ",$email;
